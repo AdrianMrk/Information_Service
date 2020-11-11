09d68263828554c211b1ce86e59ca9efd8484785
AdrianMarkiewicz <amarkiewicz27@gmail.com>
2020-11-11 15:11:47 +0100

    Change page look

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { PostsList } from "./components/PostsList";
-import { AppHeader } from "./AppHeader";
 import firebaseData from "./components/FirebaseData";
 
 
@@ -23,16 +22,25 @@ export class App extends React.Component {
   }
  
   onClickTop = () => {
-   
+    this.setState({ posts: null });
+    
+    const promisedPost = firebaseData.getTop();
+     promisedPost
+     .then(results => this.setState({ posts: results }))
+     .catch(error => console.log( error));
 
   }
   onClickNew = () => {
- 
+    this.setState({ posts: null });
+    
+    const promisedPost = firebaseData.getNew();
+     promisedPost
+     .then(results => this.setState({ posts: results }))
+     .catch(error => console.log( error));
   }
 
-  onClickNew = () => {
- 
-  }
+
+  
   chooseLang = () => {
     if(this.state.currentlanguage === "PL")
     this.setState({ currentlanguage: "ENG" })
@@ -52,16 +60,16 @@ export class App extends React.Component {
        
         <main className="ui main text container">
         
-        PL 
-        <label class="switch">
+        
+        PL  <label className="switch">
           <input type="checkbox" onClick={this.chooseLang}/>
-          <span class="slider round"></span>
-        </label>
-        ENG
+          <span className="slider round"></span>
+        </label> WORLD
+         
         <br/> <br/>
-        <button onClick={this.onClickHot}>HOT</button>;
-        <button onClick={this.onClickTop}>TOP</button>;
-        <button onClick={this.onClickNew}>NEW</button>;
+        <button onClick={this.onClickHot}>HOT</button>
+        <button onClick={this.onClickTop}>TOP</button>
+        <button onClick={this.onClickNew}>NEW</button>
         
         {posts ? <PostsList posts={posts} currentlanguage={currentlanguage}/> : 'Ładowanie…'}
          
